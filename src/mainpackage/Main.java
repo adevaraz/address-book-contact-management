f1960f7d956196e7bca7084b3ca2cc07eb8489ed
khoirunnisapk <48423384+khoirunnisapk@users.noreply.github.com>
2020-07-19 21:16:50 +0700

    haaaa

--- src/mainpackage/Main.java
+++ src/mainpackage/Main.java
@@ -10,12 +10,12 @@ import java.nio.charset.Charset;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		
+		Display display;
 		int exit = 0;
 		int answer;
 		//we will loop until user wants to exit the application
 		do {
-                        displayMenu();
+            display.displayMenu();
 			answer = choose();
 			switch(answer)
                         {
@@ -46,28 +46,18 @@ public class Main {
 		System.out.println("Application terminating...");
 	}
         
-        public static void displayMenu(){
-            
-            System.out.println("==Menu==");
-            System.out.println("1. Print Contacts");
-            System.out.println("2. Add Contact");
-            System.out.println("3. Search Contact");
-            System.out.println("4. Edit Contact");
-            System.out.println("5. Delete Contact");
-            System.out.println("0. Exit");
-        }
         
-        public static int choose(){
-            int answer;
-            Scanner input = new Scanner(System.in);
+    public static int choose(){
+        int answer;
+        Scanner input = new Scanner(System.in);
             
-            System.out.print("Pilih : ");
-            try {//we handle the input of the user
-		answer = input.nextInt();
-            } catch (NumberFormatException e) {
+        System.out.print("Pilih : ");
+        try {//we handle the input of the user
+        	answer = input.nextInt();
+        } catch (NumberFormatException e) {
                 //e.printStackTrace();
-		answer = -1;
-            }
-            return answer;
+        	answer = -1;
         }
+            return answer;
+    }
 }
